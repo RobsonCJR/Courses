f1c0a41de35578ce8d325b12c2bf72531995fe19
RobsonCJR <robsonsilvajunior23@gmail.com>
2021-01-07 19:53:38 -0300

    Adicionando alterações

---
+++ javascript_course/aula_15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,4,3,2,1]
+
+num.push(6) // Adiciona um elemento ao final do vetor
+num.sort() // ordena os números
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do vetor é: ${num[0]}`);
+let pos = num.indexOf(3)
+console.log(`O valor 3 está na posição ${pos}`);
